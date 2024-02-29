a230e760d5ac9fbba3c47670f018c507bb17ac53
tramcrazy <81013186+tramcrazy@users.noreply.github.com>
2024-02-29 17:53:28 +0000

    Build initial JS

---
+++ main.js
@@ -0,0 +1,11 @@
+function submitForm() {
+    let form = document.getElementById("mainForm");
+    let submitButton = document.getElementById("submitButton");
+    submitButton.disabled = true;
+    submitButton.innerHTML = "Please wait...";
+    Pageclip.form(form, {
+        onResponse: function (error, response) {
+            window.location.href = "google.com";
+        }
+    });
+}
