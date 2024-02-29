922b9f0d69686e4a6727d6d6aa4231953d027db3
tramcrazy <81013186+tramcrazy@users.noreply.github.com>
2024-02-29 17:58:54 +0000

    Update JS

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function submitForm() {
+/*function submitForm() {
     let form = document.getElementById("mainForm");
     let submitButton = document.getElementById("submitButton");
     submitButton.disabled = true;
@@ -8,4 +8,10 @@ function submitForm() {
             window.location.href = "google.com";
         }
     });
-}
+}*/
+let form = document.getElementById("mainForm");
+Pageclip.form(form, {
+    onResponse: function (error, response) {
+        window.location.href = "https://google.com";
+    }
+});
