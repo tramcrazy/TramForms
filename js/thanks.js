b426599fbcb10d92e8ccc5744e35ac062b9d83f3
tramcrazy <81013186+tramcrazy@users.noreply.github.com>
2024-03-06 12:51:10 +0000

    Add validation and local storage purging, remove debug code

--- js/thanks.js
+++ js/thanks.js
@@ -1,5 +1,7 @@
 formName = localStorage.getItem("completedForm");
-console.log("form name: " + formName);
-document.title = formName + " | TramForms";
-document.getElementById("formName").innerHTML = formName;
-document.getElementById("thanksText").innerHTML = "Thanks for completing " + formName + " on TramForms. The submission was successful!";
+if (!(formName == null)) {
+    document.title = formName + " | TramForms";
+    document.getElementById("formName").innerHTML = formName;
+    document.getElementById("thanksText").innerHTML = "Thanks for completing " + formName + " on TramForms. The submission was successful!";
+    localStorage.removeItem("completedForm");
+}
