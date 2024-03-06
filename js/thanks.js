b485bd596e0d198dbc0666a5634463437b656444
tramcrazy <81013186+tramcrazy@users.noreply.github.com>
2024-03-06 12:46:57 +0000

    Add debug console logging

--- js/thanks.js
+++ js/thanks.js
@@ -1,4 +1,5 @@
 formName = localStorage.getItem("completedForm");
+console.log("form name: " + formName);
 document.title = formName + " | TramForms";
 document.getElementById("formName").innerHTML = formName;
 document.getElementById("thanksText").innerHTML = "Thanks for completing " + formName + " on TramForms. The submission was successful!";
